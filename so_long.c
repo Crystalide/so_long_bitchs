f4fa016c40dc6df1d45dca992494b3581ca6754b
Nastasia Dufourneau <ndufourn@f1r1s8.codam.nl>
2025-03-07 12:01:06 +0100

    testing my git skillz

--- so_long.c
+++ so_long.c
@@ -6,12 +6,14 @@
 /*   By: ndufourn <ndufourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 14:31:34 by ndufourn          #+#    #+#             */
-/*   Updated: 2025/02/07 15:41:39 by ndufourn         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:54:05 by ndufourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/include/MLX42/MLX42.h"
 #include "so_long.h"
+// #include <math.h>
+// #define RED 
 
 int	main(void)
 {
@@ -20,6 +22,15 @@ int	main(void)
 	mlx = mlx_init(1080, 720, "SO_LONG - MA GAME", true);
 	if (!mlx)
 		return(ft_printf("%s", "MLX42 initialization failed\n"));
+	mlx_image_t *img = mlx_new_image(mlx, 1080, 720);
+	// for (size_t i = 0; i < 1080; i++)
+	// {
+	// 	for (size_t j = 0; j < 720; j++)
+	// 	{
+	// 		mlx_put_pixel(img, i, j, rand());	
+	// 	}	
+	// }
+	mlx_image_to_window(mlx, img, 0, 0);
 	mlx_loop_hook(mlx, ft_close_window, mlx);
 	mlx_close_hook(mlx, ft_close_window, mlx);
 	mlx_loop(mlx);
