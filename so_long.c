1e2f87bdc8f482cbb620e8542644314c1c244d4f
root <root@LAPTOP-BNOD6R26.>
2025-03-18 19:36:53 +0100

    i have a background now

--- so_long.c
+++ so_long.c
@@ -12,31 +12,45 @@
 
 #include "MLX42/include/MLX42/MLX42.h"
 #include "so_long.h"
-#include <math.h>
-#define RED
 
 int	main(void)
 {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	mlx_texture_t	*texture;
+	unsigned char	*pixel;
 	size_t		i;
 	size_t		j;
 
 	mlx = mlx_init(IMG_WIDTH, IMG_HEIGHT, "SO_LONG - MA GAME", true);
 	if (!mlx)
-		return (ft_printf("%s", "MLX42 initialization failed\n"));
+		return (ft_printf("%s", "Failed to initialize MLX42\n"));
+	
+	texture = mlx_load_png("textures/spacebackground.png");
+	if (!texture)
+		return (ft_printf("%s", "Failed to load background\n"));
+
 	img = mlx_new_image(mlx, IMG_WIDTH, IMG_HEIGHT);
+	if (!img)
+		return (ft_printf("%s", "Failed to create image\n"));
+	
 	i = 0;
 	while (i < IMG_WIDTH)
 	{
 		j = 0;
 		while (j < IMG_HEIGHT)
 		{
-			mlx_put_pixel(img, i, j, rand());
+			pixel = texture->pixels +
+				((j * texture->height / IMG_HEIGHT) * texture->width +
+				(i * texture->width / IMG_WIDTH)) * texture->bytes_per_pixel;
+			
+			mlx_put_pixel(img, i, j,
+				(pixel[0] << 24) | (pixel[1] << 16) | (pixel[2] << 8) | pixel[3]);
 			j++;
 		}
 		i++;
 	}
+
 	mlx_image_to_window(mlx, img, 0, 0);
 	mlx_loop_hook(mlx, ft_close_window, mlx);
 	mlx_close_hook(mlx, ft_close_window, mlx);
